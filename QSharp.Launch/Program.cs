954bd80d9e855eec673c8a98c3e52476fb7a9ba3
Javi Kroonenburg <Javi.Kroonenburg@ordina.nl>
2020-05-04 12:22:15 +0200

    Added projects

---
+++ QSharp.Launch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QSharp.Launch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
